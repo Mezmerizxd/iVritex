08ba5485c6144674b246b8dfdb18841298b16d4d
Rory Pearson <52637194+Mezmerizxd@users.noreply.github.com>
2023-12-06 16:43:04 +0000

    aaa

--- iVritex/src/frontend/purge_theme/purge_ui.cpp
+++ iVritex/src/frontend/purge_theme/purge_ui.cpp
@@ -7,12 +7,14 @@
 #include "ChooseOption.hpp"
 #include "NumberOption.hpp"
 
-namespace big::Ui1 {
+namespace big::Ui1
+{
 
 	void Ui::PurgeTheme()
 	{
 		// Home
-		g_ui.AddSubmenu<RegularSubmenu>("Home", Home, [](RegularSubmenu* sub) {
+		g_ui.AddSubmenu<RegularSubmenu>("Home", Home, [](RegularSubmenu *sub)
+																		{
 			sub->AddOption<SubOption>("Self", nullptr, Self);	
 			sub->AddOption<SubOption>("Network", nullptr, Network);
 			sub->AddOption<SubOption>("Weapons", nullptr, Weapons);
@@ -21,27 +23,29 @@ namespace big::Ui1 {
 			sub->AddOption<SubOption>("Teleport", nullptr, Teleport);
 			sub->AddOption<SubOption>("Recovery", nullptr, Recovery);
 			sub->AddOption<SubOption>("Credits", nullptr, Credits);
-			sub->AddOption<SubOption>("Settings", nullptr, Settings);
-			});
-		
+			sub->AddOption<SubOption>("Settings", nullptr, Settings); });
+
 		// Self
-		g_ui.AddSubmenu<RegularSubmenu>("Self", Self, [](RegularSubmenu* sub) {
+		g_ui.AddSubmenu<RegularSubmenu>("Self", Self, [](RegularSubmenu *sub)
+																		{
 			sub->AddOption<BoolOption<bool>>("Godmode", nullptr, &features::game.Self_Godmode, BoolDisplay::DefaultToggles);
-			sub->AddOption<BoolOption<bool>>("No Clip", nullptr, &features::game.Self_Noclip, BoolDisplay::DefaultToggles);
-			});
-		
+			sub->AddOption<BoolOption<bool>>("No Clip", nullptr, &features::game.Self_Noclip, BoolDisplay::DefaultToggles); });
+
 		// Settings
-		g_ui.AddSubmenu<RegularSubmenu>("Settings", Settings, [](RegularSubmenu* sub) {
-			//sub->AddOption<NumberOption<std::uint8_t>>("[Theme] R", nullptr, &g_Ui->iVritex_Color.r, '\0', static_cast<std::uint8_t>(255));
-			//sub->AddOption<NumberOption<std::uint8_t>>("[Theme] G", nullptr, &g_Ui->iVritex_Color.g, '\0', static_cast<std::uint8_t>(255));
-			//sub->AddOption<NumberOption<std::uint8_t>>("[Theme] B", nullptr, &g_Ui->iVritex_Color.b, '\0', static_cast<std::uint8_t>(255));
-			});
+		g_ui.AddSubmenu<RegularSubmenu>("Settings", Settings, [](RegularSubmenu *sub)
+																		{
+																			// sub->AddOption<NumberOption<std::uint8_t>>("[Theme] R", nullptr, &g_Ui->iVritex_Color.r, '\0', static_cast<std::uint8_t>(255));
+																			// sub->AddOption<NumberOption<std::uint8_t>>("[Theme] G", nullptr, &g_Ui->iVritex_Color.g, '\0', static_cast<std::uint8_t>(255));
+																			// sub->AddOption<NumberOption<std::uint8_t>>("[Theme] B", nullptr, &g_Ui->iVritex_Color.b, '\0', static_cast<std::uint8_t>(255));
+																		});
 	}
 
 }
 
-namespace big::Ui1 {
-	void Ui::Tick() {
+namespace big::Ui1
+{
+	void Ui::Tick()
+	{
 		std::lock_guard lock(m_Mutex);
 
 		CheckForInput();
@@ -83,7 +87,6 @@ namespace big::Ui1 {
 			DrawFooter();
 			DrawDescription();
 		}
-
 	}
 
 	void Ui::HandleInput()
@@ -206,10 +209,10 @@ namespace big::Ui1 {
 		{
 		case HeaderType::Static:
 			DrawRect(
-				m_PosX,
-				m_DrawBaseY + (m_HeaderHeight / 2.f), m_Width,
-				m_HeaderHeight,
-				iVritex_Color);
+					m_PosX,
+					m_DrawBaseY + (m_HeaderHeight / 2.f), m_Width,
+					m_HeaderHeight,
+					iVritex_Color);
 			break;
 		case HeaderType::Custom:
 			GRAPHICS::DRAW_SPRITE("Themes", "PurgeTheme", m_PosX, m_DrawBaseY + (m_HeaderHeight / 2.f), m_Width, m_HeaderHeight, 0.f, 255, 255, 255, 255);
@@ -218,23 +221,23 @@ namespace big::Ui1 {
 			for (std::size_t i = 0; i < (m_HeaderGradientTransparent ? 1 : 20); ++i)
 			{
 				DrawSprite(
-					"aircraft_dials",
-					"aircraft_dials_g0",
-					m_PosX - (m_HeaderGradientStretch / 2.f),
-					m_DrawBaseY + (m_HeaderHeight / 2.f),
-					m_Width + m_HeaderGradientStretch + m_HeaderGradientFiller,
-					m_HeaderHeight,
-					m_HeaderGradientColorRight,
-					m_HeaderGradientFlip ? 0.f : 180.f);
+						"aircraft_dials",
+						"aircraft_dials_g0",
+						m_PosX - (m_HeaderGradientStretch / 2.f),
+						m_DrawBaseY + (m_HeaderHeight / 2.f),
+						m_Width + m_HeaderGradientStretch + m_HeaderGradientFiller,
+						m_HeaderHeight,
+						m_HeaderGradientColorRight,
+						m_HeaderGradientFlip ? 0.f : 180.f);
 				DrawSprite(
-					"aircraft_dials",
-					"aircraft_dials_g0",
-					m_PosX + (m_HeaderGradientStretch / 2.f),
-					m_DrawBaseY + (m_HeaderHeight / 2.f),
-					m_Width + m_HeaderGradientStretch + m_HeaderGradientFiller,
-					m_HeaderHeight,
-					m_HeaderGradientColorLeft,
-					m_HeaderGradientFlip ? 180.f : 0.f);
+						"aircraft_dials",
+						"aircraft_dials_g0",
+						m_PosX + (m_HeaderGradientStretch / 2.f),
+						m_DrawBaseY + (m_HeaderHeight / 2.f),
+						m_Width + m_HeaderGradientStretch + m_HeaderGradientFiller,
+						m_HeaderHeight,
+						m_HeaderGradientColorLeft,
+						m_HeaderGradientFlip ? 180.f : 0.f);
 			}
 			break;
 		}
@@ -242,104 +245,106 @@ namespace big::Ui1 {
 		if (m_HeaderText)
 		{
 			DrawCenteredText(
-				"Menu",
-				m_PosX,
-				m_DrawBaseY + (m_HeaderHeight / 2.f) - (GetTextHeight(m_HeaderFont, m_HeaderTextSize) / 2.f),
-				m_HeaderTextSize,
-				m_HeaderFont,
-				m_HeaderTextColor,
-				false, true);
+					"Menu",
+					m_PosX,
+					m_DrawBaseY + (m_HeaderHeight / 2.f) - (GetTextHeight(m_HeaderFont, m_HeaderTextSize) / 2.f),
+					m_HeaderTextSize,
+					m_HeaderFont,
+					m_HeaderTextColor,
+					false, true);
 		}
 
 		m_DrawBaseY += m_HeaderHeight;
 	}
-	void Ui::DrawSubmenuBar(AbstractSubmenu* sub)
+	void Ui::DrawSubmenuBar(AbstractSubmenu *sub)
 	{
 		char leftText[64] = {};
 		std::strncpy(&leftText[0], sub->GetName(), sizeof(leftText) - 1);
-		std::transform(std::begin(leftText), std::end(leftText), std::begin(leftText), [](char c) { return static_cast<char>(toupper(c)); });
+		std::transform(std::begin(leftText), std::end(leftText), std::begin(leftText), [](char c)
+									 { return static_cast<char>(toupper(c)); });
 
 		char rightText[32] = {};
 		std::snprintf(rightText, sizeof(rightText) - 1, "%zu / %zu", sub->GetSelectedOption() + 1, sub->GetNumOptions());
 
 		DrawRect(
-			m_PosX,
-			m_DrawBaseY + (m_SubmenuBarHeight / 2.f),
-			m_Width, m_SubmenuBarHeight,
-			m_SubmenuBarBackgroundColor);
+				m_PosX,
+				m_DrawBaseY + (m_SubmenuBarHeight / 2.f),
+				m_Width, m_SubmenuBarHeight,
+				m_SubmenuBarBackgroundColor);
 		DrawLeftText(
-			&leftText[0],
-			m_PosX - (m_Width / m_SubmenuBarPadding),
-			m_DrawBaseY + (m_SubmenuBarHeight / 2.f) - (GetTextHeight(m_SubmenuBarFont, m_SubmenuBarTextSize) / 1.5f),
-			m_SubmenuBarTextSize, m_SubmenuBarFont,
-			iVritex_Color,
-			false, true);
+				&leftText[0],
+				m_PosX - (m_Width / m_SubmenuBarPadding),
+				m_DrawBaseY + (m_SubmenuBarHeight / 2.f) - (GetTextHeight(m_SubmenuBarFont, m_SubmenuBarTextSize) / 1.5f),
+				m_SubmenuBarTextSize, m_SubmenuBarFont,
+				iVritex_Color,
+				false, true);
 
 		m_DrawBaseY += m_SubmenuBarHeight;
 	}
-	void Ui::DrawOption(AbstractOption* opt, bool selected)
+	void Ui::DrawOption(AbstractOption *opt, bool selected)
 	{
 		DrawRect( // Option background
-			m_PosX,
-			m_DrawBaseY + (m_OptionHeight / 2.f),
-			m_Width,
-			m_OptionHeight,
-			m_OptionUnselectedBackgroundColor);
+				m_PosX,
+				m_DrawBaseY + (m_OptionHeight / 2.f),
+				m_Width,
+				m_OptionHeight,
+				m_OptionUnselectedBackgroundColor);
 
-		for (std::size_t i = 0; i < (m_OptionGradientTransparent ? 1 : 20); ++i) {
+		for (std::size_t i = 0; i < (m_OptionGradientTransparent ? 1 : 20); ++i)
+		{
 			DrawSprite(
-				"aircraft_dials",
-				"aircraft_dials_g0",
-				m_PosX - (m_OptionGradientStretch / 2.f),
-				m_DrawBaseY + (m_OptionHeight / 2.f),
-				m_Width + m_OptionGradientStretch + m_OptionGradientFiller,
-				m_OptionHeight,
-				selected ? m_OptionGradientColorRight : m_OptionUnselectedScrollColor,
-				m_OptionGradientFlip ? 0.f : 180.f);
+					"aircraft_dials",
+					"aircraft_dials_g0",
+					m_PosX - (m_OptionGradientStretch / 2.f),
+					m_DrawBaseY + (m_OptionHeight / 2.f),
+					m_Width + m_OptionGradientStretch + m_OptionGradientFiller,
+					m_OptionHeight,
+					selected ? m_OptionGradientColorRight : m_OptionUnselectedScrollColor,
+					m_OptionGradientFlip ? 0.f : 180.f);
 			DrawSprite(
-				"aircraft_dials",
-				"aircraft_dials_g0",
-				m_PosX - (m_OptionGradientStretch / 2.f),
-				m_DrawBaseY + (m_OptionHeight / 2.f),
-				m_Width + m_OptionGradientStretch + m_OptionGradientFiller,
-				m_OptionHeight,
-				selected ? m_OptionGradientColorLeft : m_OptionUnselectedScrollColor,
-				m_OptionGradientFlip ? 180.f : 0.f);
+					"aircraft_dials",
+					"aircraft_dials_g0",
+					m_PosX - (m_OptionGradientStretch / 2.f),
+					m_DrawBaseY + (m_OptionHeight / 2.f),
+					m_Width + m_OptionGradientStretch + m_OptionGradientFiller,
+					m_OptionHeight,
+					selected ? m_OptionGradientColorLeft : m_OptionUnselectedScrollColor,
+					m_OptionGradientFlip ? 180.f : 0.f);
 		}
 
-		DrawRect( // Scroller
-			m_PosX - 0.0975f, //0.6875f
-			m_DrawBaseY + (m_OptionHeight / 2.f),
-			0.005f,
-			m_OptionHeight,
-			selected ? m_OptionSelectedScrollColor : m_OptionUnselectedScrollColor);
+		DrawRect(							// Scroller
+				m_PosX - 0.0975f, // 0.6875f
+				m_DrawBaseY + (m_OptionHeight / 2.f),
+				0.005f,
+				m_OptionHeight,
+				selected ? m_OptionSelectedScrollColor : m_OptionUnselectedScrollColor);
 		DrawLeftText( // Option text
-			opt->GetLeftText(),
-			m_PosX - (m_Width / m_OptionPadding) + 0.0035f,
-			m_DrawBaseY + (m_OptionHeight / 2.f) - (GetTextHeight(m_OptionFont, m_OptionTextSize) / 1.5f),
-			m_OptionTextSize,
-			m_OptionFont,
-			selected ? m_OptionSelectedTextColor : m_OptionUnselectedTextColor,
-			false, false);
+				opt->GetLeftText(),
+				m_PosX - (m_Width / m_OptionPadding) + 0.0035f,
+				m_DrawBaseY + (m_OptionHeight / 2.f) - (GetTextHeight(m_OptionFont, m_OptionTextSize) / 1.5f),
+				m_OptionTextSize,
+				m_OptionFont,
+				selected ? m_OptionSelectedTextColor : m_OptionUnselectedTextColor,
+				false, false);
 		DrawRightText( // Arrows
-			opt->GetRightText(),
-			m_PosX + (m_Width / m_OptionPadding),
-			m_DrawBaseY + (m_OptionHeight / 2.f) - (GetTextHeight(m_OptionFont, m_OptionTextSize) / 1.5f),
-			m_OptionTextSize,
-			m_OptionFont,
-			selected ? m_OptionSelectedTextColor : m_OptionUnselectedTextColor,
-			false, false);
+				opt->GetRightText(),
+				m_PosX + (m_Width / m_OptionPadding),
+				m_DrawBaseY + (m_OptionHeight / 2.f) - (GetTextHeight(m_OptionFont, m_OptionTextSize) / 1.5f),
+				m_OptionTextSize,
+				m_OptionFont,
+				selected ? m_OptionSelectedTextColor : m_OptionUnselectedTextColor,
+				false, false);
 
 		if (opt->GetFlag(OptionFlag::Enterable))
 		{
 			DrawRightText(
-				">>",
-				m_PosX + (m_Width / m_OptionPadding),
-				m_DrawBaseY + (m_OptionHeight / 2.f) - (GetTextHeight(Font::Monospace, m_OptionTextSize) / 1.725f),
-				m_OptionTextSize,
-				Font::Monospace,
-				selected ? m_OptionSelectedTextColor : m_OptionUnselectedTextColor,
-				false, false);
+					">>",
+					m_PosX + (m_Width / m_OptionPadding),
+					m_DrawBaseY + (m_OptionHeight / 2.f) - (GetTextHeight(Font::Monospace, m_OptionTextSize) / 1.725f),
+					m_OptionTextSize,
+					Font::Monospace,
+					selected ? m_OptionSelectedTextColor : m_OptionUnselectedTextColor,
+					false, false);
 		}
 
 		m_DrawBaseY += m_OptionHeight;
@@ -347,24 +352,24 @@ namespace big::Ui1 {
 	void Ui::DrawFooter()
 	{
 		DrawRect(
-			m_PosX,
-			m_DrawBaseY + (m_FooterHeight / 2.f),
-			m_Width,
-			m_FooterHeight,
-			m_FooterBackgroundColor);
+				m_PosX,
+				m_DrawBaseY + (m_FooterHeight / 2.f),
+				m_Width,
+				m_FooterHeight,
+				m_FooterBackgroundColor);
 
 		DrawLeftText( // Option text
-			"By SpetsnazSicario",
-			m_PosX - (m_Width / m_OptionPadding),
-			m_DrawBaseY + (m_OptionHeight / 2.f) - (GetTextHeight(m_OptionFont, m_OptionTextSize) / 1.3f),
-			m_FooterTextSize,
-			m_FooterFont,
-			m_FooterTextColor,
-			false, false);
+				"By Mezmerizxd",
+				m_PosX - (m_Width / m_OptionPadding),
+				m_DrawBaseY + (m_OptionHeight / 2.f) - (GetTextHeight(m_OptionFont, m_OptionTextSize) / 1.3f),
+				m_FooterTextSize,
+				m_FooterFont,
+				m_FooterTextColor,
+				false, false);
 
 		float size = m_FooterSpriteSize;
 		float rotation = 0.f;
-		const char* texture = "shop_arrows_upanddown";
+		const char *texture = "shop_arrows_upanddown";
 
 		if (!m_SubmenuStack.empty())
 		{
@@ -386,20 +391,20 @@ namespace big::Ui1 {
 		auto sizee = GetSpriteScale(size);
 
 		DrawSprite(
-			"commonmenu",
-			texture,
-			m_PosX + 0.09f,
-			m_DrawBaseY + (m_FooterHeight / 2.f),
-			sizee.x,
-			sizee.y,
-			m_FooterSpriteColor,
-			rotation);
+				"commonmenu",
+				texture,
+				m_PosX + 0.09f,
+				m_DrawBaseY + (m_FooterHeight / 2.f),
+				sizee.x,
+				sizee.y,
+				m_FooterSpriteColor,
+				rotation);
 
 		m_DrawBaseY += m_FooterHeight;
 	}
 	void Ui::DrawDescription()
 	{
-		const char* description{};
+		const char *description{};
 
 		if (!m_SubmenuStack.empty())
 		{
@@ -419,32 +424,30 @@ namespace big::Ui1 {
 		m_DrawBaseY += m_DescriptionHeightPadding;
 
 		DrawRect(
-			m_PosX,
-			m_DrawBaseY + (m_DescriptionHeight / 2.f),
-			m_Width,
-			m_DescriptionHeight,
-			m_DescriptionBackgroundColor);
+				m_PosX,
+				m_DrawBaseY + (m_DescriptionHeight / 2.f),
+				m_Width,
+				m_DescriptionHeight,
+				m_DescriptionBackgroundColor);
 
 		auto spriteSize = GetSpriteScale(m_DescriptionSpriteSize);
 		DrawSprite(
-			"shared", "info_icon_32",
-			m_PosX - (m_Width / m_DescriptionPadding) + (spriteSize.x / 2.f),
-			m_DrawBaseY + (m_DescriptionHeight / 2.f),
-			spriteSize.x,
-			spriteSize.y,
-			m_DescriptionSpriteColor,
-			0.f
-		);
+				"shared", "info_icon_32",
+				m_PosX - (m_Width / m_DescriptionPadding) + (spriteSize.x / 2.f),
+				m_DrawBaseY + (m_DescriptionHeight / 2.f),
+				spriteSize.x,
+				spriteSize.y,
+				m_DescriptionSpriteColor,
+				0.f);
 
 		DrawLeftText(
-			description,
-			m_PosX - (m_Width / m_DescriptionPadding) + (spriteSize.x * 1.15f),
-			m_DrawBaseY + (m_DescriptionHeight / 2.f) - (GetTextHeight(m_DescriptionFont, m_DescriptionTextSize) / 1.5f),
-			m_DescriptionTextSize,
-			m_DescriptionFont,
-			m_DescriptionTextColor,
-			false, false
-		);
+				description,
+				m_PosX - (m_Width / m_DescriptionPadding) + (spriteSize.x * 1.15f),
+				m_DrawBaseY + (m_DescriptionHeight / 2.f) - (GetTextHeight(m_DescriptionFont, m_DescriptionTextSize) / 1.5f),
+				m_DescriptionTextSize,
+				m_DescriptionFont,
+				m_DescriptionTextColor,
+				false, false);
 
 		m_DrawBaseY += m_DescriptionHeight;
 	}
@@ -453,7 +456,7 @@ namespace big::Ui1 {
 	{
 		GRAPHICS::DRAW_RECT(x, y, width, height, color.r, color.g, color.b, color.a);
 	}
-	void Ui::DrawSprite(const char* dict, const char* texture, float x, float y, float width, float height, Color color, float rotation)
+	void Ui::DrawSprite(const char *dict, const char *texture, float x, float y, float width, float height, Color color, float rotation)
 	{
 		if (GRAPHICS::HAS_STREAMED_TEXTURE_DICT_LOADED(dict))
 		{
@@ -464,7 +467,7 @@ namespace big::Ui1 {
 			GRAPHICS::REQUEST_STREAMED_TEXTURE_DICT(dict, false);
 		}
 	}
-	void Ui::DrawLeftText(const char* text, float x, float y, float size, Font font, Color color, bool outline, bool shadow)
+	void Ui::DrawLeftText(const char *text, float x, float y, float size, Font font, Color color, bool outline, bool shadow)
 	{
 		UI::SET_TEXT_SCALE(size, size);
 		UI::SET_TEXT_FONT(static_cast<int>(font));
@@ -478,13 +481,13 @@ namespace big::Ui1 {
 		UI::ADD_TEXT_COMPONENT_SUBSTRING_PLAYER_NAME(text);
 		UI::END_TEXT_COMMAND_DISPLAY_TEXT(x, y);
 	}
-	void Ui::DrawRightText(const char* text, float x, float y, float size, Font font, Color color, bool outline, bool shadow)
+	void Ui::DrawRightText(const char *text, float x, float y, float size, Font font, Color color, bool outline, bool shadow)
 	{
 		UI::SET_TEXT_WRAP(0.f, x);
 		UI::SET_TEXT_RIGHT_JUSTIFY(true);
 		DrawLeftText(text, x, y, size, font, color, outline, shadow);
 	}
-	void Ui::DrawCenteredText(const char* text, float x, float y, float size, Font font, Color color, bool outline, bool shadow)
+	void Ui::DrawCenteredText(const char *text, float x, float y, float size, Font font, Color color, bool outline, bool shadow)
 	{
 		UI::SET_TEXT_CENTRE(true);
 		DrawLeftText(text, x, y, size, font, color, outline, shadow);
@@ -499,9 +502,7 @@ namespace big::Ui1 {
 		int y;
 		GRAPHICS::_GET_ACTIVE_SCREEN_RESOLUTION(&x, &y);
 
-		return { (static_cast<float>(y) / static_cast<float>(x)) * size, size };
+		return {(static_cast<float>(y) / static_cast<float>(x)) * size, size};
 	}
 
 }
-
-
